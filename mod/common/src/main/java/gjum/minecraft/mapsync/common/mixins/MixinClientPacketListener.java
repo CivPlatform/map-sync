dc0d5a3b0e12273823138280e3dfb5ee2f645a66
Gjum <code.gjum@gmail.com>
2022-05-11 03:33:05 +0200

    fix mixin crash (reflection)

--- mod/common/src/main/java/gjum/minecraft/mapsync/common/mixins/MixinClientPacketListener.java
+++ mod/common/src/main/java/gjum/minecraft/mapsync/common/mixins/MixinClientPacketListener.java
@@ -3,34 +3,17 @@ package gjum.minecraft.mapsync.common.mixins;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.SectionPos;
 import net.minecraft.network.protocol.game.*;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.lang.reflect.Field;
 
 import static gjum.minecraft.mapsync.common.MapSyncMod.getMod;
 import static gjum.minecraft.mapsync.common.Utils.printErrorRateLimited;
 
 @Mixin(ClientPacketListener.class)
 public abstract class MixinClientPacketListener {
-
-	@Nullable
-	private static Field posField;
-
-	static {
-		try {
-			//noinspection JavaReflectionMemberAccess
-			posField = ClientboundSectionBlocksUpdatePacket.class.getField("sectionPos");
-			posField.setAccessible(true);
-		} catch (NoSuchFieldException e) {
-			e.printStackTrace();
-		}
-	}
 
 	@Inject(method = "handleLogin", at = @At("RETURN"))
 	protected void onHandleLogin(ClientboundLoginPacket packet, CallbackInfo ci) {
@@ -62,17 +45,6 @@ public abstract class MixinClientPacketListener {
 		}
 	}
 
-	@Inject(method = "handleChunkBlocksUpdate", at = @At("RETURN"))
-	protected void onHandleChunkBlocksUpdate(ClientboundSectionBlocksUpdatePacket packet, CallbackInfo ci) {
-		if (!Minecraft.getInstance().isSameThread()) return; // will be called again on mc thread in a moment
-		try {
-			if (posField == null) return;
-			var pos = (SectionPos) posField.get(packet);
-			getMod().handleMcChunkPartialChange(pos.x(), pos.z());
-		} catch (Throwable e) {
-			printErrorRateLimited(e);
-		}
-	}
 
 	@Inject(method = "handleBlockUpdate", at = @At("RETURN"))
 	protected void onHandleBlockUpdate(ClientboundBlockUpdatePacket packet, CallbackInfo ci) {

b1fb995cddbef76ac6541d2e901da6e04c87cb17
Gjum <code.gjum@gmail.com>
2022-05-23 03:51:41 +0200

    client close connection on unknown server packet id

--- mod/common/src/main/java/gjum/minecraft/mapsync/common/net/ServerPacketDecoder.java
+++ mod/common/src/main/java/gjum/minecraft/mapsync/common/net/ServerPacketDecoder.java
@@ -23,6 +23,7 @@ public class ServerPacketDecoder extends ReplayingDecoder<Void> {
 		if (packet == null) {
 			TcpClient.logger.error("[ServerPacketDecoder] " +
 					"Unknown server packet id " + id + " 0x" + Integer.toHexString(id));
+			ctx.close();
 			return;
 		}
 		out.add(packet);

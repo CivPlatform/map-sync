3ffcf96282b86222899c9e1677b23b5612d883aa
Gjum <code.gjum@gmail.com>
2022-05-31 04:40:21 +0200

    change version format

--- mod/common/src/main/java/gjum/minecraft/mapsync/common/MapSyncMod.java
+++ mod/common/src/main/java/gjum/minecraft/mapsync/common/MapSyncMod.java
@@ -25,7 +25,7 @@ import java.util.stream.Collectors;
 import static gjum.minecraft.mapsync.common.Cartography.chunkTileFromLevel;
 
 public abstract class MapSyncMod {
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "${version}";
 
 	private static final Minecraft mc = Minecraft.getInstance();
 

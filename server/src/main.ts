cdb3c3e3cae267b2429b1c6d02d1b0bebb67d398
Alexander <protonull@protonmail.com>
2023-02-03 09:01:32 +0000

    Add todo list in main file (easiest place to find)

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,3 +7,7 @@ Promise.resolve().then(async () => {
     await connectDB();
     const server = new TcpServer();
 });
+
+// TODO: Fix mod resource filtering so the version is correctly set
+
+// TODO: Fix mod not disconnecting from MapSync when disconnected from MC server

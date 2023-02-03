3ffb283e05fa4098a84e6dd1506fb45c1b71eedf
Alexander <protonull@protonmail.com>
2023-02-03 19:25:34 +0000

    Update to using datasource

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,31 +1,31 @@
 import "reflect-metadata";
-import { BaseEntity, Connection, createConnection } from "typeorm";
+import { BaseEntity, DataSource } from "typeorm";
 
 import { DATA_FOLDER } from "../config/mod";
 const SQLITE_PATH = process.env["SQLITE_PATH"] ?? `${DATA_FOLDER}/db.sqlite`;
 
-let db: Promise<Connection> | null = null;
+let db: DataSource | null = null;
 
 // TODO accept any EntitySchema<any>[]
 const entities: Record<string, typeof BaseEntity> = {};
 export function registerEntity(entity: typeof BaseEntity) {
     entities[entity.name] = entity;
-    if (db) db.then((db) => db.close());
+    if (db) db.destroy();
     db = null; // require reconnecting to apply entity
 }
 
-export function connectDB() {
+export async function connectDB() {
     if (!db) {
-        db = createConnection({
-            entities: Object.values(entities),
-            synchronize: true,
+        db = await new DataSource({
             type: "sqlite",
-            database: SQLITE_PATH
-        });
+            database: SQLITE_PATH,
+            synchronize: true,
+            entities: entities
+        }).initialize();
     }
     return db;
 }
 
 export async function closeDB() {
-    if (db) await db.then((db) => db.close());
+    if (db) db.destroy();
 }

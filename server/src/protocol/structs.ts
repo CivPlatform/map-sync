b2f9ea5c1738ba2f2347d83259e67daa27eccadf
Alexander <protonull@protonmail.com>
2023-02-06 15:33:33 +0000

    Add timestamped type

--- server/src/protocol/structs.ts
+++ server/src/protocol/structs.ts
@@ -3,6 +3,10 @@ export interface Pos2D {
     readonly z: number;
 }
 
+export interface Timestamped {
+    readonly timestamp: bigint;
+}
+
 export interface RegionTimestamp extends Pos2D {
     readonly ts: number; // TODO: Change this to a bigint at some point
 }

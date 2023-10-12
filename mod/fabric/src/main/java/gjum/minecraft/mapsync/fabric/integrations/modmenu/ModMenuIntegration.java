dd79f945b2670fc5cabc628682de3a5125b3a23d
Alexander <protonull@protonmail.com>
2023-10-12 18:23:05 +0100

    dammit, spaces

--- mod/fabric/src/main/java/gjum/minecraft/mapsync/fabric/integrations/modmenu/ModMenuIntegration.java
+++ mod/fabric/src/main/java/gjum/minecraft/mapsync/fabric/integrations/modmenu/ModMenuIntegration.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
  * Adds support for https://github.com/TerraformersMC/ModMenu (Fabric only)
  */
 public class ModMenuIntegration implements ModMenuApi {
-    public @NotNull ConfigScreenFactory<ModGui> getModConfigScreenFactory() {
-        return ModGui::new;
-    }
+	public @NotNull ConfigScreenFactory<ModGui> getModConfigScreenFactory() {
+		return ModGui::new;
+	}
 }

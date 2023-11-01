952d1bee7509b09ae7b09d4ab97fe7f710b4189e
Alexander <protonull@protonmail.com>
2023-11-01 08:55:15 +0000

    Fixup client encryption handling
    This does away with EncryptionTranslator and its cached buffers, instead allocating what it needs and leaning on the cipher itself to produce the resulting buffer.

--- mod/common/src/main/java/gjum/minecraft/mapsync/common/net/encryption/EncryptionDecoder.java
+++ mod/common/src/main/java/gjum/minecraft/mapsync/common/net/encryption/EncryptionDecoder.java
@@ -1,31 +1,44 @@
 package gjum.minecraft.mapsync.common.net.encryption;
 
+import gjum.minecraft.mapsync.common.net.Packet;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
-
-import javax.crypto.Cipher;
-import javax.crypto.ShortBufferException;
-import javax.crypto.spec.IvParameterSpec;
 import java.security.GeneralSecurityException;
 import java.security.Key;
 import java.util.List;
+import javax.crypto.Cipher;
+import javax.crypto.ShortBufferException;
+import javax.crypto.spec.IvParameterSpec;
+import org.apache.commons.lang3.ArrayUtils;
+import org.jetbrains.annotations.NotNull;
 
 public class EncryptionDecoder extends MessageToMessageDecoder<ByteBuf> {
-	private final EncryptionTranslator decryptionCodec;
+	private final Cipher cipher;
 
-	public EncryptionDecoder(Key key) {
+	public EncryptionDecoder(
+			final @NotNull Key key
+	) {
 		try {
-			Cipher cipher = Cipher.getInstance("AES/CFB8/NoPadding");
-			cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(key.getEncoded()));
-			decryptionCodec = new EncryptionTranslator(cipher);
-		} catch (GeneralSecurityException e) {
-			throw new RuntimeException(e);
+			this.cipher = Cipher.getInstance("AES/CFB8/NoPadding");
+			this.cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(key.getEncoded()));
+		}
+		catch (final GeneralSecurityException thrown) {
+			throw new IllegalStateException(thrown);
 		}
 	}
 
 	@Override
-	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws ShortBufferException {
-		out.add(decryptionCodec.decipher(ctx, in));
+	protected void decode(
+			final ChannelHandlerContext ctx,
+			final ByteBuf in,
+			final List<Object> out
+	) throws ShortBufferException {
+		final byte[] receivedBytes = Packet.readByteArrayOfSize(in, in.readableBytes());
+		final byte[] decryptedBytes = this.cipher.update(receivedBytes);
+		if (ArrayUtils.getLength(decryptedBytes) > 0) {
+			out.add(Unpooled.wrappedBuffer(decryptedBytes));
+		}
 	}
 }
--- mod/common/src/main/java/gjum/minecraft/mapsync/common/net/encryption/EncryptionEncoder.java
+++ mod/common/src/main/java/gjum/minecraft/mapsync/common/net/encryption/EncryptionEncoder.java
@@ -1,30 +1,42 @@
 package gjum.minecraft.mapsync.common.net.encryption;
 
+import gjum.minecraft.mapsync.common.net.Packet;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-
+import java.security.GeneralSecurityException;
+import java.security.Key;
 import javax.crypto.Cipher;
 import javax.crypto.ShortBufferException;
 import javax.crypto.spec.IvParameterSpec;
-import java.security.GeneralSecurityException;
-import java.security.Key;
+import org.apache.commons.lang3.ArrayUtils;
+import org.jetbrains.annotations.NotNull;
 
 public class EncryptionEncoder extends MessageToByteEncoder<ByteBuf> {
-	private final EncryptionTranslator encryptionCodec;
+	private final Cipher cipher;
 
-	public EncryptionEncoder(Key key) {
+	public EncryptionEncoder(
+			final @NotNull Key key
+	) {
 		try {
-			Cipher cipher = Cipher.getInstance("AES/CFB8/NoPadding");
-			cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(key.getEncoded()));
-			encryptionCodec = new EncryptionTranslator(cipher);
-		} catch (GeneralSecurityException e) {
-			throw new RuntimeException(e);
+			this.cipher = Cipher.getInstance("AES/CFB8/NoPadding");
+			this.cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(key.getEncoded()));
 		}
-	}
+		catch (final GeneralSecurityException thrown) {
+			throw new IllegalStateException(thrown);
+		}
+    }
 
 	@Override
-	protected void encode(ChannelHandlerContext ctx, ByteBuf in, ByteBuf out) throws ShortBufferException {
-		encryptionCodec.encipher(in, out);
+	protected void encode(
+			final ChannelHandlerContext ctx,
+			final ByteBuf in,
+			final ByteBuf out
+	) throws ShortBufferException {
+		final byte[] sendingBytes = Packet.readByteArrayOfSize(in, in.readableBytes());
+		final byte[] encryptedBytes = this.cipher.update(sendingBytes);
+		if (ArrayUtils.getLength(encryptedBytes) > 0) {
+			out.writeBytes(encryptedBytes);
+		}
 	}
 }

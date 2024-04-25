a8e80ec4989a73fe73cfde33d08a5f5bf366b0d3
Alexander <protonull@protonmail.com>
2024-04-25 10:36:08 +0100

    Fix javax annotations compilation error

--- mod/common/src/main/java/gjum/minecraft/mapsync/common/net/packet/ClientboundEncryptionRequestPacket.java
+++ mod/common/src/main/java/gjum/minecraft/mapsync/common/net/packet/ClientboundEncryptionRequestPacket.java
@@ -2,8 +2,8 @@ package gjum.minecraft.mapsync.common.net.packet;
 
 import gjum.minecraft.mapsync.common.net.Packet;
 import io.netty.buffer.ByteBuf;
+import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.Nonnull;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
@@ -15,12 +15,10 @@ import java.security.spec.X509EncodedKeySpec;
 public class ClientboundEncryptionRequestPacket implements Packet {
 	public static final int PACKET_ID = 2;
 
-	@Nonnull
-	public final PublicKey publicKey;
-	@Nonnull
-	public final byte[] verifyToken;
+	public final @NotNull PublicKey publicKey;
+	public final byte @NotNull [] verifyToken;
 
-	public ClientboundEncryptionRequestPacket(@Nonnull PublicKey publicKey, @Nonnull byte[] verifyToken) {
+	public ClientboundEncryptionRequestPacket(@NotNull PublicKey publicKey, byte @NotNull [] verifyToken) {
 		this.publicKey = publicKey;
 		this.verifyToken = verifyToken;
 	}
